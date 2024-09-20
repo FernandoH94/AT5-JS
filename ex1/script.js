8bde7a3dc931c16a5c7c05f15d62eeff68096557
FernandoH94 <fernando.hsousa@senacsp.edu.br>
2024-09-20 19:49:24 -0300

    feat: excercio 1

---
+++ ex1/script.js
@@ -0,0 +1,11 @@
+/* Crie um algoritmo de pagamento em que se casdastre os prestadores de serviço, 
+pegando os seguintes dados: nome completo, um número do PIS/PASEP, o valor da sua hora trabalhada e a quantidade
+de horas trabalhadas de 5 a 50 pessoas. No fim dos cadastros deve-se mostrar os dados, o valor bruto do serviço e
+o líquido descontando na página html dos cadastrados.
+Todos os campos devem ser válidos e cumprir os requisitos mínimos de validação.
+Ovalor da hora: mínimo de 20 reais e máximo de 500 reais.
+A quantidade de horas sempre estará entre 20 e 200 horas trabaçhadas no mês.
+O valor líquido do prestador de serviço tem descontos de impostos a recolher sendo 
+o INSS (tabela), ISS (5%) E IR (tabela).
+*/
+
